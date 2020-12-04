0cfa1c16dcd5cd39e7514333b41839234002310b
Simon Keith <simon.keith@shippeo.com>
2020-12-04 12:28:48 +0100

    day 4

---
+++ aoc2020/puzzle/day4.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+import re
+from dataclasses import dataclass, fields
+from itertools import chain, groupby
+from typing import Dict, Optional, Sequence
+
+from aoc2020.input import get_puzzle_input
+
+
+@dataclass
+class Passeport:
+    _HGT_PATTERN = re.compile(r"^(\d+)(\w{2})$")
+    _HGT_RULES = {"in": ("59", "76"), "cm": ("150", "193")}
+    _HCL_PATTERN = re.compile(r"^(#[0-9a-f]{6})$")
+    _ECL_SET = frozenset(["amb", "blu", "brn", "gry", "grn", "hzl", "oth"])
+    _PID_PATTERN = re.compile(r"^(\d{9})$")
+
+    @classmethod
+    def validate_byr(cls, v: str) -> str:
+        assert "1920" <= v <= "2002"
+        return v
+
+    @classmethod
+    def validate_iyr(cls, v: str) -> str:
+        assert "2010" <= v <= "2020"
+        return v
+
+    @classmethod
+    def validate_eyr(cls, v: str) -> str:
+        assert "2020" <= v <= "2030"
+        return v
+
+    @classmethod
+    def validate_hgt(cls, v: str) -> str:
+        match = cls._HGT_PATTERN.match(v)
+        assert match is not None
+        value, unit = match.groups()
+        assert unit in cls._HGT_RULES
+        min_value, max_value = cls._HGT_RULES[unit]
+        assert min_value <= value <= max_value
+        return v
+
+    @classmethod
+    def validate_hcl(cls, v: str) -> str:
+        assert cls._HCL_PATTERN.match(v) is not None
+        return v
+
+    @classmethod
+    def validate_ecl(cls, v: str) -> str:
+        assert v in cls._ECL_SET
+        return v
+
+    @classmethod
+    def validate_pid(cls, v: str) -> str:
+        assert cls._PID_PATTERN.match(v) is not None
+        return v
+
+    @classmethod
+    def validate_cid(cls, v: Optional[str]) -> Optional[str]:
+        return v
+
+    byr: str  # (Birth Year)
+    iyr: str  # (Issue Year)
+    eyr: str  # (Expiration Year)
+    hgt: str  # (Height)
+    hcl: str  # (Hair Color)
+    ecl: str  # (Eye Color)
+    pid: str  # (Passport ID)
+    cid: Optional[str] = None  # (Country ID)
+
+    def __post_init__(self):
+        self.byr = self.validate_byr(self.byr)
+        self.iyr = self.validate_iyr(self.iyr)
+        self.eyr = self.validate_eyr(self.eyr)
+        self.hgt = self.validate_hgt(self.hgt)
+        self.hcl = self.validate_hcl(self.hcl)
+        self.ecl = self.validate_ecl(self.ecl)
+        self.pid = self.validate_pid(self.pid)
+        self.cid = self.validate_cid(self.cid)
+
+
+def parse_puzzle_input(puzzle_input: Sequence[str]) -> Sequence[Dict[str, str]]:
+    return tuple(
+        dict(
+            field.split(":")
+            for field in chain.from_iterable(line.split() for line in v)
+        )
+        for k, v in groupby(puzzle_input, lambda x: len(x) > 0)
+        if k
+    )
+
+
+def solve_first_part(puzzle_input: Sequence[str]) -> int:
+    passeports = parse_puzzle_input(puzzle_input)
+    expected_fields = set(f.name for f in fields(Passeport)) - {"cid"}
+    return sum(expected_fields <= p.keys() for p in passeports)
+
+
+def solve_second_part(puzzle_input: Sequence[str]) -> int:
+    passeports = parse_puzzle_input(puzzle_input)
+    valid_count = 0
+    for kwargs in passeports:
+        try:
+            Passeport(**kwargs)
+        except (TypeError, AssertionError):
+            pass
+        else:
+            valid_count += 1
+    return valid_count
+
+
+if __name__ == "__main__":
+    puzzle_input = get_puzzle_input("day4")
+    print(solve_first_part(puzzle_input))
+    print(solve_second_part(puzzle_input))
---
+++ tests/test_day4.py
@@ -0,0 +1,93 @@
+from typing import Sequence
+
+from aoc2020.puzzle.day4 import Passeport, solve_first_part, solve_second_part
+from pytest import fixture, raises
+
+
+@fixture
+def puzzle_input() -> Sequence[str]:
+    return (
+        "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd",
+        "byr:1937 iyr:2017 cid:147 hgt:183cm",
+        "",
+        "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884",
+        "hcl:#cfa07d byr:1929",
+        "",
+        "hcl:#ae17e1 iyr:2013",
+        "eyr:2024",
+        "ecl:brn pid:760753108 byr:1931",
+        "hgt:179cm",
+        "",
+        "hcl:#cfa07d eyr:2025 pid:166559648",
+        "iyr:2011 ecl:brn hgt:59in",
+    )
+
+
+def test_first_part(puzzle_input):
+    assert solve_first_part(puzzle_input) == 2
+
+
+@fixture
+def puzzle_input_invalid() -> Sequence[str]:
+    return (
+        "eyr:1972 cid:100",
+        "hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+        "",
+        "iyr:2019",
+        "hcl:#602927 eyr:1967 hgt:170cm",
+        "ecl:grn pid:012533040 byr:1946",
+        "",
+        "hcl:dab227 iyr:2012",
+        "ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+        "",
+        "hgt:59cm ecl:zzz",
+        "eyr:2038 hcl:74454a iyr:2023",
+        "pid:3556412378 byr:2007",
+    )
+
+
+@fixture
+def puzzle_input_valid() -> Sequence[str]:
+    return (
+        "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980",
+        "hcl:#623a2f",
+        "",
+        "eyr:2029 ecl:blu cid:129 byr:1989",
+        "iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+        "",
+        "hcl:#888785",
+        "hgt:164cm byr:2001 iyr:2015 cid:88",
+        "pid:545766238 ecl:hzl",
+        "eyr:2022",
+        "",
+        "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719",
+    )
+
+
+def test_second_part(puzzle_input_invalid, puzzle_input_valid):
+    assert Passeport.validate_byr("2002")
+    with raises(AssertionError):
+        Passeport.validate_byr("2003")
+
+    assert Passeport.validate_hgt("60in")
+    assert Passeport.validate_hgt("190cm")
+    with raises(AssertionError):
+        Passeport.validate_hgt("190in")
+        Passeport.validate_hgt("190")
+
+    assert Passeport.validate_hcl("#123abc")
+    with raises(AssertionError):
+        Passeport.validate_hcl("#123abz")
+        Passeport.validate_hcl("123abc")
+
+    assert Passeport.validate_ecl("brn")
+    with raises(AssertionError):
+        Passeport.validate_hcl("wat")
+
+    assert Passeport.validate_pid("000000001")
+    with raises(AssertionError):
+        Passeport.validate_pid("0123456789")
+
+    assert solve_second_part(puzzle_input_invalid) == 0
+
+    assert solve_second_part(puzzle_input_valid) == 4
