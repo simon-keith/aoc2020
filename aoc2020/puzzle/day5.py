caafc801ad94fdc417bbbc701d981f7aa2ae2429
Simon Keith <simon.keith@shippeo.com>
2020-12-09 00:23:55 +0100

    simpler day 5

--- aoc2020/puzzle/day5.py
+++ aoc2020/puzzle/day5.py
@@ -1,56 +1,22 @@
 #!/usr/bin/env python
-from typing import Sequence, Tuple
+from typing import Sequence
 
 from aoc2020.input import get_puzzle_input
 
-SEAT_FRONT = {"F": True, "B": False}
-SEAT_LEFT = {"L": True, "R": False}
+PUZZLE_MAPPING = {"F": "0", "B": "1", "L": "0", "R": "1"}
 
 
-def parse_puzzle_input(puzzle_input: Sequence[str]) -> Sequence[Tuple[str, str]]:
-    return tuple((p[:7], p[7:]) for p in puzzle_input)
-
-
-def split_range(r: range) -> Tuple[range, range]:
-    range_length = len(r)
-    if range_length < 2 or range_length % 2 != 0:
-        raise ValueError(f"{r} is not splittable")
-    diff = r.stop - r.start
-    split_point = r.start + int(diff / 2)
-    return range(r.start, split_point, r.step), range(split_point, r.stop, r.step)
-
-
-def walk_directions(directions: Sequence[bool]) -> int:
-    r = range(2 ** len(directions))
-    for d in directions:
-        if d:
-            r, _ = split_range(r)
-        else:
-            _, r = split_range(r)
-    return next(iter(r))
-
-
-def decode_seat(rows: str, columns: str) -> Tuple[int, int]:
-    row = walk_directions([SEAT_FRONT[r] for r in rows])
-    column = walk_directions([SEAT_LEFT[c] for c in columns])
-    return row, column
-
-
-def get_seat_id(row: int, column: int) -> int:
-    return row * 8 + column
+def parse_puzzle_input(puzzle_input: Sequence[str]) -> Sequence[int]:
+    return tuple(int("".join(PUZZLE_MAPPING[c] for c in p), 2) for p in puzzle_input)
 
 
 def solve_first_part(puzzle_input: Sequence[str]) -> int:
-    seat_list = parse_puzzle_input(puzzle_input)
-    return max(get_seat_id(r, c) for r, c in (decode_seat(r, c) for r, c in seat_list))
+    return max(parse_puzzle_input(puzzle_input))
 
 
 def solve_second_part(puzzle_input: Sequence[str]) -> int:
-    seat_list = parse_puzzle_input(puzzle_input)
-    seat_id_set = set(
-        get_seat_id(r, c) for r, c in (decode_seat(r, c) for r, c in seat_list)
-    )
-    for seat_id in range(min(seat_id_set), max(seat_id_set) + 1):
+    seat_id_set = set(parse_puzzle_input(puzzle_input))
+    for seat_id in range(min(seat_id_set) + 1, max(seat_id_set)):
         if (
             seat_id not in seat_id_set
             and seat_id + 1 in seat_id_set
--- tests/test_day5.py
+++ tests/test_day5.py
@@ -1,16 +1,13 @@
 from typing import Sequence
 
-from aoc2020.puzzle.day5 import decode_seat, parse_puzzle_input, solve_first_part
+from aoc2020.puzzle.day5 import parse_puzzle_input
 from pytest import fixture
 
 
 @fixture
 def puzzle_input() -> Sequence[str]:
-    return ("FBFBBFFRLR", "FBFBBFFRLR", "BFFFBBFRRR", "FFFBBBFRRR", "BBFFBBFRLL")
+    return ("FBFBBFFRLR", "BFFFBBFRRR", "FFFBBBFRRR", "BBFFBBFRLL")
 
 
-def test_first_part(puzzle_input):
-    seat_list = parse_puzzle_input(puzzle_input)
-    decoded_seat_list = tuple(decode_seat(r, c) for r, c in seat_list)
-    assert decoded_seat_list == ((44, 5), (44, 5), (70, 7), (14, 7), (102, 4))
-    assert solve_first_part(puzzle_input) == 820
+def test_parsing(puzzle_input):
+    assert parse_puzzle_input(puzzle_input) == (357, 567, 119, 820)
