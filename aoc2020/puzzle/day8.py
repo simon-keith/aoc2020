0c0050bb565b73276f347322ebb26c47ad41ada9
Simon Keith <simon.keith@shippeo.com>
2020-12-08 23:34:15 +0100

    init day 8

---
+++ aoc2020/puzzle/day8.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from typing import Sequence
+
+from aoc2020.input import get_puzzle_input
+
+
+def solve_first_part(puzzle_input: Sequence[str]) -> int:
+    return
+
+
+def solve_second_part(puzzle_input: Sequence[str]) -> int:
+    return
+
+
+if __name__ == "__main__":
+    puzzle_input = get_puzzle_input("day8")
+    print(solve_first_part(puzzle_input))
+    print(solve_second_part(puzzle_input))
---
+++ tests/test_day8.py
@@ -0,0 +1,17 @@
+from typing import Sequence
+
+from aoc2020.puzzle.day8 import solve_first_part, solve_second_part
+from pytest import fixture
+
+
+@fixture
+def puzzle_input() -> Sequence[str]:
+    return ()
+
+
+def test_first_part(puzzle_input):
+    assert solve_first_part(puzzle_input) == 0
+
+
+def test_second_part(puzzle_input):
+    assert solve_second_part(puzzle_input) == 0
