173172ccd4e375aff36affac257f7822be12355b
Simon Keith <simon.keith@shippeo.com>
2020-12-10 00:41:02 +0100

    day 9

--- aoc2020/puzzle/day9.py
+++ aoc2020/puzzle/day9.py
@@ -2,14 +2,58 @@
 from typing import Sequence
 
 from aoc2020.input import get_puzzle_input
+from aoc2020.puzzle.day1 import iter_pairs_that_sum_to_target
+
+PUZZLE_PREAMBLE = 25
 
 
-def solve_first_part(puzzle_input: Sequence[str]) -> int:
-    return
+def parse_puzzle_input(puzzle_input: Sequence[str]) -> Sequence[int]:
+    return tuple(int(p) for p in puzzle_input)
 
 
-def solve_second_part(puzzle_input: Sequence[str]) -> int:
-    return
+def find_weakness(integers: Sequence[int], preamble: int) -> int:
+    reference = set(integers[:preamble])
+    for i in range(preamble, len(integers)):
+        target = integers[i]
+        try:
+            next(iter_pairs_that_sum_to_target(reference, target))
+        except StopIteration:
+            return target
+        old, new = integers[i - preamble], integers[i]
+        reference.remove(old)
+        reference.add(new)
+    raise ValueError("did not find any weakness")
+
+
+def find_contiguous_tuple(integers: Sequence[int], target: int) -> Sequence[int]:
+    lower, upper = 0, 2
+    total = integers[lower] + integers[upper - 1]
+    while upper <= len(integers):
+        if total < target:
+            total += integers[upper]
+            upper += 1
+        elif total > target:
+            total -= integers[lower]
+            lower += 1
+        else:
+            return tuple(integers[lower:upper])
+    raise ValueError("did not find any continuous tuple")
+
+
+def solve_first_part(
+    puzzle_input: Sequence[str], preamble: int = PUZZLE_PREAMBLE
+) -> int:
+    integers = parse_puzzle_input(puzzle_input)
+    return find_weakness(integers, preamble)
+
+
+def solve_second_part(
+    puzzle_input: Sequence[str], preamble: int = PUZZLE_PREAMBLE
+) -> int:
+    integers = parse_puzzle_input(puzzle_input)
+    weakness = find_weakness(integers, preamble)
+    contiguous_tuple = find_contiguous_tuple(integers, weakness)
+    return min(contiguous_tuple) + max(contiguous_tuple)
 
 
 if __name__ == "__main__":
--- tests/test_day9.py
+++ tests/test_day9.py
@@ -6,12 +6,33 @@ from pytest import fixture
 
 @fixture
 def puzzle_input() -> Sequence[str]:
-    return ()
+    return (
+        "35",
+        "20",
+        "15",
+        "25",
+        "47",
+        "40",
+        "62",
+        "55",
+        "65",
+        "95",
+        "102",
+        "117",
+        "150",
+        "182",
+        "127",
+        "219",
+        "299",
+        "277",
+        "309",
+        "576",
+    )
 
 
 def test_first_part(puzzle_input):
-    assert solve_first_part(puzzle_input) == 0
+    assert solve_first_part(puzzle_input, 5) == 127
 
 
 def test_second_part(puzzle_input):
-    assert solve_second_part(puzzle_input) == 0
+    assert solve_second_part(puzzle_input, 5) == 62
