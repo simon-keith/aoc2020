535991deed69df3bca27495792b4d6137949428e
Simon Keith <simonkth@gmail.com>
2020-12-14 21:17:46 +0100

    init day 11

---
+++ aoc2020/puzzle/day11.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from typing import Sequence
+
+from aoc2020.input import get_puzzle_input
+
+
+def solve_first_part(puzzle_input: Sequence[str]) -> int:
+    return -1
+
+
+def solve_second_part(puzzle_input: Sequence[str]) -> int:
+    return -1
+
+
+if __name__ == "__main__":
+    puzzle_input = get_puzzle_input("day11")
+    print(solve_first_part(puzzle_input))
+    print(solve_second_part(puzzle_input))
---
+++ tests/test_day11.py
@@ -0,0 +1,28 @@
+from typing import Sequence
+
+from aoc2020.puzzle.day01 import solve_first_part, solve_second_part
+from pytest import fixture
+
+
+@fixture
+def puzzle_input() -> Sequence[str]:
+    return (
+        "L.LL.LL.LL",
+        "LLLLLLL.LL",
+        "L.L.L..L..",
+        "LLLL.LL.LL",
+        "L.LL.LL.LL",
+        "L.LLLLL.LL",
+        "..L.L.....",
+        "LLLLLLLLLL",
+        "L.LLLLLL.L",
+        "L.LLLLL.LL",
+    )
+
+
+def test_first_part(puzzle_input):
+    assert solve_first_part(puzzle_input) == 0
+
+
+def test_second_part(puzzle_input):
+    assert solve_second_part(puzzle_input) == 0
